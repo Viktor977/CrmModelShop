76a69f4a14bfd6c8e70584df6c75e2d043804557
Viktor Stepanov <angelinavirta@gmail.com>
2021-12-29 17:49:31 +0200

    beginning of the computers modeling process

---
+++ CrmBl/Model/ShopComputerModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBl.Model
+{
+    public class ShopComputerModel
+    {
+        Generator generator = new Generator();
+        public List<CashDesk> CashDesks { get; set; }
+        public List<Cart> Carts { get; set; }
+        public List<Check> Checks { get; set; } 
+        public List<Sell>Sells { get; set; }
+        public Queue<Seller> Sellers { get; set; }
+        public ShopComputerModel()
+        {
+            var sellers = generator.GetNewSellers(20);
+            generator.GetNewProducts(1000);
+            generator.GetNewCustomers(100);
+            foreach (var seller in sellers)
+            {
+                Sellers.Enqueue(seller);
+            }
+            for(int i = 0; i < 3; ++i)
+            {
+                CashDesks.Add(new CashDesk(CashDesks.Count, Sellers.Dequeue()));
+            }
+        }
+
+        public void Start()
+        {
+            var customers=generator.GetNewCustomers(10);
+
+        }
+    }
+}
